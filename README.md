12c1ec11ca10220b6d3d1800cf1bb27e21608cc0
bradkillins <bradkillins@gmail.com>
2021-04-21 12:00:07 -0600

    added styling to webapp

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# example-webapp-BVCIDUserName
+# example-webapp-zsafarialamoti097
+
+### This is a very simple website / webapp that is running on a apache server inside of a docker container
+
+### The main purpose of this is to test an automated CI/CD pipeline using AWS, Jenkins, Docker and Github
+
+**Team members:**  
+Brad Killins  
+Aman Dhiman  
+Gurbir Kaur  
+Zahra Safarialamoti
--- www/index.php
+++ www/index.php
@@ -1,11 +1,45 @@
 <?
 
-echo"
-    <h1>Hello Dima!!!</h1>
-    <p>This is our final project :)</p>
-    <p> Brad - Aman - Gurbir - Zahra </p>
+echo'
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CI/CD Pipeline Webapp</title>
+  <style>
+    body {
+      background-color: #1e1e1e;
+      color: lightblue;
+    }
+    h1 {
+      margin: 2em;
+    }
+    p {
+      margin: 1em;
+    }
+  </style>
+</head>
+<body>
+  <h1>Hello Dima!!!</h1>
+  <p>
+    This is a very simple website / webapp that is running on an apache server
+    inside of a docker container.
+  </p>
+  <p>
+    The main purpose of this is to test an automated CI/CD pipeline using AWS,
+    Jenkins, Docker and Github.
+  </p>
+  <br />
+  <p>
+    Team members: Brad Killins - Aman Dhiman - Gurbir Kaur - Zahra
+    Safarialamoti
+  </p>
+  <br />
+  <p>I hope everyone is having an awesome day! :)</p>
+</body>
+</html>
 
-    <p>I hope everyone is having an awesome day!</p>
-";
+';
 
 ?>
